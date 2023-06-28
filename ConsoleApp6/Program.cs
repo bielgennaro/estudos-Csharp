71c9fcee85077985279b1e9730ca4127d7c28e69
bielgennaro <zirondigabriel@gmail.com>
2023-06-28 03:40:35 -0300

    🚀

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp6;
+
+internal class ConversaoImplicita
+{
+    static void Main(string[] args)
+    {
+        double a;
+        float b;
+
+        a = 5.1;
+        b = (float)a;
+    }
+}
