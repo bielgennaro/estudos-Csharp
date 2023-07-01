bdf42e4da92fb93ce3043a6c8b92d3ff03e938ce
bielgennaro <zirondigabriel@gmail.com>
2023-07-01 05:08:18 -0300

    foguete num tem ré

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp15;
+
+internal class While
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número: ");
+        double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while (x >= 0.0)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+            Console.Write("Digite outro número: ");
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        Console.WriteLine("Você digitou um número inválido!");
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp16;
+
+internal class For
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Quantos números inteiros você vai digitar?");
+        int n = int.Parse(Console.ReadLine());
+
+        int soma = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write("Valor: #{0}: ", i);
+            int valor = int.Parse(Console.ReadLine());
+            soma += valor;
+        }
+
+        Console.WriteLine("Soma = " + soma);
+    }
+}
