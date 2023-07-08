147a7837e9e40a98894300771431f377a507da84
bielgennaro <zirondigabriel@gmail.com>
2023-07-08 10:25:46 -0300

    foguete num tem ré

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -4,28 +4,19 @@ namespace ConsoleApp22;
 
 internal class Program
 {
-    private static double Pi = 3.14;
-
     private static void Main(string[] args)
     {
+        var calc = new Calculadora();
+
+
         Console.WriteLine("Entre o valor do raio: ");
         var raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        var circ = Circunferencia(raio);
-        var volume = Volume(raio);
+        var circ = calc.Circunferencia(raio);
+        var volume = calc.Volume(raio);
 
         Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
         Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-        Console.WriteLine("PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-
-        double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * r * Math.Pow(r, 3.0);
-        }
+        Console.WriteLine("PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
     }
 }
