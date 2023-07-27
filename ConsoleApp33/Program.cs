b0494ce68163fdec77a4ad9469fc939ebd33cfcc
bielgennaro <zirondigabriel@gmail.com>
2023-07-27 20:48:51 -0300

    DateTimes v2

---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp33;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TimeSpan t1 = TimeSpan.MaxValue;
+        TimeSpan t2 = TimeSpan.Zero;
+        TimeSpan t3 = TimeSpan.MinValue;
+
+        TimeSpan t = new TimeSpan();
+
+        Console.WriteLine("Days: " + t.Days);
+        Console.WriteLine("Seconds: " + t.Seconds);
+    }
+}
+
